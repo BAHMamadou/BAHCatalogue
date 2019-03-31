db39f70c42c1d78b5dc36f19c84782d73f7703a1
samsung <bahpita92@gmail.com>
2019-03-31 05:00:12 +0200

    first commit

---
+++ src/test/java/org/bah/aliou/BahAliouCatalogueApplicationTests.java
@@ -0,0 +1,16 @@
+package org.bah.aliou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BahAliouCatalogueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
